1e0f5ecf2044f47b0bf5565fed2accb2af9c798e
Vedant <vedant10vaidya@gmail.com>
2025-03-15 02:38:37 +0900

    Employee portal | Spring MVC CRUD app | initial config completed

---
+++ src/main/java/empportal/controllers/MainController.java
@@ -0,0 +1,15 @@
+package empportal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		return "index";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<%@ include file="./base.jsp" %>
+		<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+	</head>
+	<body>
+		<h2>Hello World!</h2>
+	</body>
+</html>
